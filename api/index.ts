16fd0013306a33f93bde7750073d3432c176385f
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-13 21:38:57 +0200

    Add dependencies and update API endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,27 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { createClient } from "@libsql/client";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+console.log("Server started!");
+
+if (!process.env.TURSO_DATABASE_URL) throw new Error("TURSO_DATABASE_URL is not set");
+if (!process.env.TURSO_AUTH_TOKEN) throw new Error("TURSO_AUTH_TOKEN is not set");
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const turso = createClient({
+  url: process.env.TURSO_DATABASE_URL as string,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+const app = new Hono().basePath("/api");
 
-export default handle(app)
+app.get("/", async (c) => {
+  console.log("GET /");
+  const users = await turso.execute("SELECT * FROM users");
+  return c.json({ message: "Hello Hono!", users });
+});
+
+export default handle(app);

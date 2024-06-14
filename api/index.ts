bddf57bb1b51c78a36ebcd8fbc33105080524013
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-06-14 02:46:05 +0200

    Add contact form endpoint and error handling

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,76 @@
-import { createClient } from "@libsql/client";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
+import { v4 as uuidv4 } from "uuid";
+import turso from "./db";
 
 console.log("Server started!");
-
-if (!process.env.TURSO_DATABASE_URL) throw new Error("TURSO_DATABASE_URL is not set");
-if (!process.env.TURSO_AUTH_TOKEN) throw new Error("TURSO_AUTH_TOKEN is not set");
 
 export const config = {
   runtime: "edge",
 };
 
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL as string,
-  authToken: process.env.TURSO_AUTH_TOKEN,
-});
-
 const app = new Hono().basePath("/api");
 
-app.get("/", async (c) => {
-  console.log("GET /");
-  const users = await turso.execute("SELECT * FROM users");
-  const messages = await turso.execute("SELECT * FROM messages");
-  return c.json({ message: "Hello Hono!", users, messages });
+app.use(
+  "/contact",
+  cors({
+    origin: ["http://localhost:5173"],
+    allowMethods: ["POST"],
+    allowHeaders: ["Access-Control-Allow-Origin"],
+  })
+);
+
+type ContactFormType = {
+  tel: string;
+  email: string;
+  msg: string;
+  hp?: string;
+};
+
+type ErrorTableArgs = [string, string, string];
+type MessagesTableArgs = [string, string, string, string];
+
+const regexp = {
+  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  tel: /^\d{10}$/,
+};
+
+const hasLength = (str: string) => str.length > 0;
+const isValidEmail = (email: string) => hasLength(email) && regexp.email.test(email);
+const isValidTel = (tel: string) => hasLength(tel) && regexp.tel.test(tel);
+
+app.all("/contact", async (c) => {
+  console.log(`all : [${c.req.method}] /contact`);
+  const res = { authorized: false, success: false };
+  const id = uuidv4();
+  const { email, msg, tel, hp } = (await c.req.json()) as ContactFormType;
+
+  const errors = [
+    !isValidEmail(email) && "Invalid email",
+    !isValidTel(tel) && "Invalid tel",
+    !hasLength(msg) && "Invalid message",
+    hp && "Invalid HP",
+  ];
+
+  if (errors.some((e) => e)) {
+    console.log("Invalid form", { email, msg, tel });
+    const _ = await turso.execute({
+      sql: "INSERT INTO error VALUES (?, ?, ?)",
+      /* id : varchar, field : varchar, content : varchar */
+      args: [id, "contact", errors.filter((e) => e).join(", ")] as ErrorTableArgs,
+    });
+    return c.json({ ...res, errors });
+  }
+
+  const _ = await turso.execute({
+    sql: "INSERT INTO messages VALUES (?, ?, ?, ?)",
+    /* varchar, varchar, varchar, text */
+    args: [id, tel, email, msg] as MessagesTableArgs,
+  });
+  console.log("Inserted message", id, tel, email, msg);
+
+  return c.json({ ...res, authorized: true, success: true });
 });
 
 export default handle(app);

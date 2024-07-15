0612cb532a55d74146021624c935d8ca2181a5bd
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-15 22:36:35 +0200

    Add timeStamp function to helper.ts

--- api/helper.ts
+++ api/helper.ts
@@ -5,4 +5,15 @@ const buildId = () => {
   return [date.getHours(), date.getMinutes(), date.getSeconds(), uuidv4().slice(5)].map((d) => d.toString()).join("");
 };
 
-export { buildId };
+type TimeStamp = {
+  days?: number;
+  hours?: number;
+  minutes?: number;
+};
+const timeStamp = (time: TimeStamp) =>
+  Math.floor(Date.now() / 1000) +
+  (time.days || 0) * 24 * 60 * 60 +
+  (time.hours || 0) * 60 * 60 +
+  (time.minutes || 0) * 60;
+
+export { buildId, timeStamp };

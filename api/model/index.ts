5ddfd1ae19dbe392a7112740c3e06f97ac174c87
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-18 02:16:08 +0200

    Reorder imports and update findAll function in index.ts

--- api/model/index.ts
+++ api/model/index.ts
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client";
 import { startAgentTransaction } from "./agent";
 import { deleteErrors, getErrors, insertErrors } from "./errors";
 import { insertMessage } from "./message";
-import { getUser, findAllUsers } from "./users";
+import { findAllUsers, getUser } from "./users";
 
 if (!process.env.TURSO_DATABASE_URL) throw new Error("TURSO_DATABASE_URL is not set");
 if (!process.env.TURSO_AUTH_TOKEN) throw new Error("TURSO_AUTH_TOKEN is not set");
@@ -13,8 +13,16 @@ const turso = createClient({
 });
 
 const findAll = async (table: "agent" | "error" | "messages") => {
-  const dbRes = await turso.execute({ sql: "SELECT * FROM ?", args: [table] });
-  return dbRes;
+  switch (table) {
+    case "agent":
+      return await turso.execute("SELECT * FROM agent");
+    case "error":
+      return await turso.execute("SELECT * FROM error");
+    case "messages":
+      return await turso.execute("SELECT * FROM messages");
+    default:
+      return null;
+  }
 };
 
 export default turso;

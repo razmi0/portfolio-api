db728591506681f336e57330c6231212903feff0
razmiO <thomas.cuesta31@gmail.com>
2025-01-01 16:52:10 +0100

    Fix : login endpoint return a json resp on error

--- api/index.ts
+++ api/index.ts
@@ -17,7 +17,7 @@ import { isInvalidUser, validateContactForm, validateLoginForm } from "./validat
 
 const tkSec = process.env.TOKEN_SECRET;
 export const config = {
-  runtime: "edge",
+    runtime: "edge",
 };
 
 const app = new Hono().basePath(basePath);
@@ -26,16 +26,16 @@ app.use("/contact", cors);
 app.use("/agent", cors);
 app.use("/login", cors);
 app.use("/auth/*", authCors, async (c, next) => {
-  const clientTk = c.req.header("Authorization")?.split(" ")[1];
-  if (!clientTk) throw new HTTPException(401, { message: "Unauthorized" });
-  /**
-   * user : string
-   * exp : number
-   */
-  const decodedPayload = await verify(clientTk, tkSec as string, "HS256");
-  console.log("connection : ", decodedPayload);
-  if (!decodedPayload) throw new HTTPException(401, { message: "Unauthorized" });
-  await next();
+    const clientTk = c.req.header("Authorization")?.split(" ")[1];
+    if (!clientTk) throw new HTTPException(401, { message: "Unauthorized" });
+    /**
+     * user : string
+     * exp : number
+     */
+    const decodedPayload = await verify(clientTk, tkSec as string, "HS256");
+    console.log("connection : ", decodedPayload);
+    if (!decodedPayload) throw new HTTPException(401, { message: "Unauthorized" });
+    await next();
 });
 
 /**
@@ -45,26 +45,26 @@ app.use("/auth/*", authCors, async (c, next) => {
  * @table errors (id, field, content)
  */
 app.all("/contact", async (c) => {
-  console.log(`all : [${c.req.method}] /contact`);
+    console.log(`all : [${c.req.method}] /contact`);
 
-  const res: MinimalResponse = { authorized: false, success: false };
+    const res: MinimalResponse = { authorized: false, success: false };
 
-  const id = uuidv4();
-  const data = (await c.req.json()) as ContactFormType;
+    const id = uuidv4();
+    const data = (await c.req.json()) as ContactFormType;
 
-  const { errors, hasError } = validateContactForm(data);
+    const { errors, hasError } = validateContactForm(data);
 
-  if (hasError) {
-    const _ = await db.insertErrors(id, errors, "contact");
+    if (hasError) {
+        const _ = await db.insertErrors(id, errors, "contact");
+        return c.json(res);
+    }
+
+    const _ = await db.insertMessage({ ...data, id });
+
+    res.authorized = true;
+    res.success = true;
+
     return c.json(res);
-  }
-
-  const _ = await db.insertMessage({ ...data, id });
-
-  res.authorized = true;
-  res.success = true;
-
-  return c.json(res);
 });
 
 /**
@@ -73,18 +73,18 @@ app.all("/contact", async (c) => {
  * @table agents ( ip, created_at, updated_at, platform, city, continent, country, region, latitude, longitude, timezone)
  */
 app.all("/agent", async (c) => {
-  console.log(`all : [${c.req.method}] /agent`);
+    console.log(`all : [${c.req.method}] /agent`);
 
-  const res: MinimalResponse = { authorized: false, success: false };
-  const { platform }: Pick<UserAgentInfo, "platform"> = await c.req.json();
+    const res: MinimalResponse = { authorized: false, success: false };
+    const { platform }: Pick<UserAgentInfo, "platform"> = await c.req.json();
 
-  const success = await db.startAgentTransaction({ ...buildAgentHeader(c), platform, id: uuidv4() });
-  if (!success) return c.json(res);
+    const success = await db.startAgentTransaction({ ...buildAgentHeader(c), platform, id: uuidv4() });
+    if (!success) return c.json(res);
 
-  res.authorized = success;
-  res.success = success;
+    res.authorized = success;
+    res.success = success;
 
-  return c.json(res);
+    return c.json(res);
 });
 
 /**
@@ -93,77 +93,94 @@ app.all("/agent", async (c) => {
  * @table users (id, username, password)
  */
 app.all("/login", async (c) => {
-  console.log(`all : [${c.req.method}] /login`);
+    console.log(`all : [${c.req.method}] /login`);
 
-  const res: MinimalResponse = { authorized: false, success: false };
+    const res: MinimalResponse = { authorized: false, success: false };
 
-  const data = (await c.req.json()) as LoginFormType;
-  const formError = validateLoginForm(data);
+    const data = (await c.req.json()) as LoginFormType;
+    const formError = validateLoginForm(data);
 
-  if (formError.hasError) {
-    const _ = await db.insertErrors(uuidv4(), Object.entries(formError.errors), "login");
-    return c.json({ res, errors: formError.errors });
-  }
+    /**
+     * If an error is found, insert it into the database ( client code manipulation by the user )
+     */
+    if (formError.hasError) {
+        const _ = await db.insertErrors(uuidv4(), Object.entries(formError.errors), "login");
+        return c.json({ res, errors: formError.errors });
+    }
 
-  const user = await db.getUser(data.username, data.password);
-  const invalid = isInvalidUser(user);
-  if (invalid.hasError) throw new HTTPException(401, { message: "Invalid credentials" }); // return c.json({ res, errors: invalid }); //
+    const user = await db.getUser(data.username, data.password);
+    const invalid = isInvalidUser(user);
+    if (invalid.hasError) return c.json({ res, errors: invalid.errors });
 
-  // AUTHENTICATED USER
+    // AUTHENTICATED USER
 
-  const tsOptions = { days: 2 };
-  const ts = timeStamp(tsOptions);
-  const payload = {
-    user: data.username,
-    exp: ts.timestamp,
-  };
+    const tsOptions = { days: 2 };
+    const ts = timeStamp(tsOptions);
+    const payload = {
+        user: data.username,
+        exp: ts.timestamp,
+    };
 
-  const token = await sign(payload, tkSec as string);
+    const token = await sign(payload, tkSec as string);
 
-  setCookie(c, "token", token, {
-    httpOnly: true,
-    secure: true,
-    path: "/auth",
-    expires: ts.date,
-    maxAge: tsOptions.days * 24 * 60 * 60, // 2 days in seconds
-    sameSite: "none",
-  });
+    setCookie(c, "token", token, {
+        httpOnly: true,
+        secure: true,
+        path: "/auth",
+        expires: ts.date,
+        maxAge: tsOptions.days * 24 * 60 * 60, // 2 days in seconds
+        sameSite: "none",
+    });
 
-  res.authorized = true;
-  res.success = true;
+    res.authorized = true;
+    res.success = true;
 
-  console.log("Login success", data.username);
+    console.log("Login success", data.username);
 
-  return c.json({ res, token, payload });
+    return c.json({ res, token, payload });
 });
 
+/**
+ * @description Authenticated endpoints
+ * @method GET
+ * @table messages (id, name, email, message)
+ * @table users (id, username, password)
+ * @table agents ( ip, created_at, updated_at, platform, city, continent, country, region, latitude, longitude, timezone)
+ * @table errors (id, field, content)
+ *
+ * - /auth/msgs
+ * - /auth/users
+ * - /auth/agents
+ * - /auth/errors
+ */
+let docs;
 app.get("/auth/msgs", async (c) => {
-  console.log(`${c.req.method} /auth/msgs`);
-  const res = await db.findAll("messages");
-  if (!res) return c.json({ success: false });
-  return c.json(res.rows);
+    console.log(`${c.req.method} /auth/msgs`);
+    const res = await db.findAll("messages");
+    if (!res) return c.json({ success: false });
+    return c.json(res.rows);
 });
 
 app.get("/auth/users", async (c) => {
-  console.log(`${c.req.method} /auth/users`);
-  const res = await db.findAllUsers();
-  if (!res) return c.json({ success: false });
-  return c.json(res.rows);
+    console.log(`${c.req.method} /auth/users`);
+    const res = await db.findAllUsers();
+    if (!res) return c.json({ success: false });
+    return c.json(res.rows);
 });
 
 app.get("/auth/agents", async (c) => {
-  console.log(`${c.req.method} /auth/agents`);
-  const res = await db.findAll("agent");
-  if (!res) return c.json({ success: false });
-  console.log("res in /auth/agents ", res);
-  return c.json(res.rows);
+    console.log(`${c.req.method} /auth/agents`);
+    const res = await db.findAll("agent");
+    if (!res) return c.json({ success: false });
+    console.log("res in /auth/agents ", res);
+    return c.json(res.rows);
 });
 
 app.get("/auth/errors", async (c) => {
-  console.log(`${c.req.method} /auth/errors`);
-  const res = await db.findAll("error");
-  if (!res) return c.json({ success: false });
-  return c.json(res.rows);
+    console.log(`${c.req.method} /auth/errors`);
+    const res = await db.findAll("error");
+    if (!res) return c.json({ success: false });
+    return c.json(res.rows);
 });
 
 export default handle(app);
